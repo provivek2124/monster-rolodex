43c9e08c3c23a0dd0064c9cd9af0a9505b1a36ba
vivek kumar <provivek2124@gmail.com>
2020-04-25 12:31:17 +0530

    Updates

---
+++ precache-manifest.3afb5ab01bf113bc38c3fffbc88c2c99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ae87a5e61f64b5fede64164f5001489",
+    "url": "/MONSTER-ROLODEX/index.html"
+  },
+  {
+    "revision": "d933fc286f00d3879335",
+    "url": "/MONSTER-ROLODEX/static/css/main.3f659f98.chunk.css"
+  },
+  {
+    "revision": "9d84f5b306774dfc9f1a",
+    "url": "/MONSTER-ROLODEX/static/js/2.91e906d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MONSTER-ROLODEX/static/js/2.91e906d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d933fc286f00d3879335",
+    "url": "/MONSTER-ROLODEX/static/js/main.34295de1.chunk.js"
+  },
+  {
+    "revision": "fe8251630de2d66ba1d5",
+    "url": "/MONSTER-ROLODEX/static/js/runtime-main.918a8436.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MONSTER-ROLODEX/static/media/logo.5d5d9eef.svg"
+  }
+]);
